74a034bf10b6cb800104d48c09551e1e0a3049bb
ronilsonbatista <ronilsondaconceicao@gmail.com>
2019-04-21 11:13:04 -0300

    creating classes for lists

---
+++ BestPlaces/Features/ListPlaces/Presenter/ListPlacesPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  ListPlacesPresenter.swift
+//  BestPlaces
+//
+//  Created by Ronilson Batista on 21/04/19.
+//  Copyright © 2019 Ronilson Batista. All rights reserved.
+//
+
+import Foundation
